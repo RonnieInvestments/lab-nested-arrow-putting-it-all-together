35c6a7724cee09b152c2ee248dfc8329e572fe6a
RonnieInvestments <ronnieinvestments81@gmail.com>
2025-11-29 21:43:47 +0300

    Created login tracker function.

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
+// Login Tracker
+function createLoginTracker(userInfo) {
+  let attemptCount = 0;
+
+// Tracking login attempts
+
+  let loginAttempt = (passwordAttempt) => {
+    attemptCount++;
+
+// Successful login handling
+
+    if (passwordAttempt === userInfo.password && attemptCount <= 3) {
+      return `Login successful`;
+
+// Incorrect password handling
+
+    } else if (passwordAttempt !== userInfo.password && attemptCount <=3) {
+      return `Attempt ${attemptCount}: Login failed`;
+
+  // Lock the account after 3 failed attempts
+
+    } else if (attemptCount > 3) {
+      return `Account locked due to too many failed login attempts`;}
+  }
+  return loginAttempt;
+}
+// Dummy object for testing the function
+
+/* const userInfo = { username: 'user1', password: 'securePass' };
+let createLoginTrackerResult = createLoginTracker(userInfo);
+console.log(createLoginTrackerResult('wrongPass')); // attempt 1: Login failed */
 
 
 
