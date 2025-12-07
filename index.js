d21d16f3c6fac04c13a5571e12c6eb226db62632
RonnieInvestments <ronnieinvestments81@gmail.com>
2025-12-07 10:55:57 +0300

    Made better comments.

--- index.js
+++ index.js
@@ -1,29 +1,59 @@
 // Login Tracker
 function createLoginTracker(userInfo) {
+  
+/*
+
+This outer function will initialize the login feature.
+The userInfo parameter will be an object containing:
+  username: A string representing the username.
+  password: A string representing the user’s password.
+
+Example:
+{
+    "username": "user1",
+    "password": "password123"
+}
+  */
+
+// Initialize attempt count
+
   let attemptCount = 0;
 
-// Tracking login attempts
+// Tracking login attempts (counts each login attempt)
 
   let loginAttempt = (passwordAttempt) => {
-    attemptCount++;
+    attemptCount++; // use the increment
 
-// Successful login handling
+/* Successful login handling 
+
+Password should be correct
+Attempts should be three or less
+
+*/
 
     if (passwordAttempt === userInfo.password && attemptCount <= 3) {
       return `Login successful`;
 
-// Incorrect password handling
+/* Incorrect password handling 
+
+Incorrect password
+Attempts are three or less
+
+*/
 
     } else if (passwordAttempt !== userInfo.password && attemptCount <=3) {
       return `Attempt ${attemptCount}: Login failed`;
 
-  // Lock the account after 3 failed attempts
+// Lock the account after 3 failed attempts
 
     } else if (attemptCount > 3) {
       return `Account locked due to too many failed login attempts`;}
   }
   return loginAttempt;
+  
 }
+
+
 // Dummy object for testing the function
 
 /* const userInfo = { username: 'user1', password: 'securePass' };
